6a2b7018fa782913cef0db23079a35f670c68a47
Mateusz Kranz <mateuszkranz@Mateuszs-MacBook-Pro.local>
2016-07-03 14:30:43 -0700

    Farm GAME: Added a Shop Screen

--- Button.java
+++ Button.java
@@ -1,4 +1,65 @@
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+
 
 public class Button {
-
+	GameController game;
+	String Name;
+	int xPos;
+	int yPos;
+	int Height;
+	int Width;
+	Color boxColor;
+	String Type;
+	String Dim;
+	BufferedImage Image;
+	public Button(GameController game){
+		this(game,0,0,0,0,Color.red,"","","",null);
+	}
+	public Button(GameController game,
+			int xPos,
+			int yPos,
+			int Height,
+			int Width,
+			Color boxColor,
+			String Name,
+			String Type,
+			String Dim,
+			BufferedImage Image){
+		this.game=game;
+		this.xPos=xPos;
+		this.yPos=yPos;
+		this.Height=Height;
+		this.Width=Width;
+		this.boxColor =boxColor;
+		this.Name = Name;
+		this.Type=Type;
+		this.Image=Image;
+		this.Dim=Dim;
+	}
+	public void ClickedOn(int MxPos, int MyPos,GameController game){
+		System.out.println("RAN");
+		for(Button x : game.buttons){
+			if(x.Type.equals("DimTele")){
+				System.out.println("BEFORE CHECKING IF INSIDE");
+				if((MxPos>=x.xPos-5&&MxPos<=x.xPos-5+x.Width+10)&&(MyPos>=x.yPos-5&&MyPos<=x.yPos-5+x.Height+10)){
+					System.out.println("AFTER CHECKING IF INSIDE");
+					if(x.Name.equals("ToShop")&&game.Dimension=="Farm"){
+						System.out.print("SUCESS");
+						game.Dimension="Shop";
+						break;
+					}
+					if(x.Name.equals("ToFarm")&&game.Dimension=="Shop"){
+						game.Dimension="Farm";
+						break;
+					}
+				}
+			}
+			if(Type.equals("Purchase")){
+				
+			}
+		}
+	}
 }
--- GameController.java
+++ GameController.java
@@ -27,6 +27,7 @@ public class GameController extends JFrame{
 	Planter planter = new Planter(this);
 	GameController game = this;
 	Menu menus = new Menu(game);
+	Button but = new Button(game);
 	Collector collect = new Collector(this);
 	int counter;
 	int MouseX;
@@ -45,12 +46,14 @@ public class GameController extends JFrame{
 	ArrayList<Spaces> Spaces= new ArrayList<>();
 	ArrayList<Plants> Plants= new ArrayList<>();
 	ArrayList<Menu> menu= new ArrayList<>();
+	ArrayList<Button> buttons= new ArrayList<>();
 	ArrayList<Plants> PlantedPlants= new ArrayList<>();
 	ArrayList<Yields> TotalYield= new ArrayList<>();
 	ArrayList<Seeds> seeds= new ArrayList<>();
 	Plants selectedPlant;
 	BufferedImage FullBLUEBERRY;
 	BufferedImage Shop;
+	BufferedImage Farm;
 	BufferedImage onethirdBLUEBERRY;
 	BufferedImage twothirdBLUEBERRY;
 	public GameController(){
@@ -70,14 +73,15 @@ public class GameController extends JFrame{
 		addMouseWheelListener(Wheel);
 		setVisible(true);
 		setSize(1000,1000);
-		initObj();
 		try{
 			FullBLUEBERRY = ImageIO.read(new File("FullBLUEBERRY.jpg"));
 			Shop = ImageIO.read(new File("Shop.jpg"));
+			Farm = ImageIO.read(new File("Farm.jpg"));
 			onethirdBLUEBERRY = ImageIO.read(new File("onethirdBLUEBERRY.jpg"));
 			twothirdBLUEBERRY = ImageIO.read(new File("twothirdBLUEBERRY.jpg"));
 		}catch(IOException e){
 		}
+		initObj();
 	}
 	public void initObj(){
 		System.out.println("Obj");
@@ -96,6 +100,8 @@ public class GameController extends JFrame{
 		Plants.add(new Plants(this,0,0,0,0, "BLUEBERRY", 0,30,10,false,false));
 		menu.add(new Menu(this,100,100,100,20,0,"BLUEBERRY"));
 		seeds.add(new Seeds(this,10,"BLUEBERRY"));
+		buttons.add(new Button(this,5, 25, 50, 50,Color.orange, "ToShop","DimTele","Farm",Shop));
+		buttons.add(new Button(this,5, 25, 50, 50,Color.orange, "ToFarm","DimTele","Shop",Farm));
 	}
 	public void step(){
 		MouseControl(MouseX, MouseY);
@@ -124,6 +130,11 @@ public class GameController extends JFrame{
 		SpacesControl();
 
 	}
+	public void ButtonDraw(Graphics g, Button button){
+		g.setColor(button.boxColor);
+		g.fillRect(button.xPos-5,button.yPos-5,button.Width+10,button.Height+10);
+		g.drawImage(button.Image, button.xPos, button.yPos, button.Width, button.Height, null);
+	}
 	public void delay (int time){
 		try{
 			Thread.sleep(time);
@@ -137,74 +148,87 @@ public class GameController extends JFrame{
 		g.drawImage(onethirdBLUEBERRY,10000,10000,50,50, null);
 		g.drawImage(twothirdBLUEBERRY,10000,10000,50,50, null);
 		g.drawImage(Shop,10000,10000,50,50, null);
+		g.drawImage(Farm,10000,10000,50,50, null);
 		g.setColor(Color.gray);
 		g.fillRect(0, 0, 10000,10000);
 		if(Dimension=="Farm"){
-		if (Lands.size() > 0) {
-			g.setColor(new Color(25,150,50));
-			g.fillRect(Lands.get(0).xPos,Lands.get(0).yPos, Lands.get(0).Width*CELL_SIZE, Lands.get(0).Height*CELL_SIZE);
-		}
-		for(Yields z : TotalYield){
-			g.setColor(new Color(250,250,250));
-			g.drawString(z.Type + " : " + z.Amount,800, 50);
-		}
-		for(Seeds c : seeds){
-			g.setColor(new Color(250,250,250));
-			g.drawString(c.Type + " : " +c.Amount,800, 75);
-		}
-		for(Spaces x : Spaces){
-			if(x.plant==null){
-				//brown
-				g.setColor(new Color(200,100,0));
+			if (Lands.size() > 0) {
+				g.setColor(new Color(25,150,50));
+				g.fillRect(Lands.get(0).xPos,Lands.get(0).yPos, Lands.get(0).Width*CELL_SIZE, Lands.get(0).Height*CELL_SIZE);
+			}
+			for(Yields z : TotalYield){
+				g.setColor(new Color(250,250,250));
+				g.drawString(z.Type + " : " + z.Amount,800, 50);
+			}
+			for(Seeds c : seeds){
+				g.setColor(new Color(250,250,250));
+				g.drawString(c.Type + " seeds : " +c.Amount,800, 75);
+			}
+			for(Spaces x : Spaces){
+				if(x.plant==null){
+					//brown
+					g.setColor(new Color(200,100,0));
+					g.fillRect(x.xPos, x.yPos, x.Width, x.Height);
+					g.setColor(new Color(150,75,0));
+					g.drawRect(x.xPos, x.yPos, x.Width, x.Height);
+				}else if(x.plant.Harvestable==true){
+
+					g.drawImage(FullBLUEBERRY,x.xPos,x.yPos,x.Width,x.Height, null);
+
+				}else if(x.plant.CurrentGrowth/x.plant.GrowthTime>=(1.0/3.0)&&x.plant.CurrentGrowth/x.plant.GrowthTime<(2.0/3.0)){
+
+					g.drawImage(onethirdBLUEBERRY,x.xPos,x.yPos,x.Width,x.Height, null);
+
+				}else if(x.plant.CurrentGrowth/x.plant.GrowthTime>=(2.0/3.0)&&x.plant.CurrentGrowth/x.plant.GrowthTime<(1.0)){
+
+					g.drawImage(twothirdBLUEBERRY,x.xPos,x.yPos,x.Width,x.Height, null);
+
+				}
+			}
+			for(Menu x : menu){
+				g.setColor(Color.BLUE);
 				g.fillRect(x.xPos, x.yPos, x.Width, x.Height);
-				g.setColor(new Color(150,75,0));
-				g.drawRect(x.xPos, x.yPos, x.Width, x.Height);
-			}else if(x.plant.Harvestable==true){
-
-				g.drawImage(FullBLUEBERRY,x.xPos,x.yPos,x.Width,x.Height, null);
-
-			}else if(x.plant.CurrentGrowth/x.plant.GrowthTime>=(1.0/3.0)&&x.plant.CurrentGrowth/x.plant.GrowthTime<(2.0/3.0)){
-
-				g.drawImage(onethirdBLUEBERRY,x.xPos,x.yPos,x.Width,x.Height, null);
-
-			}else if(x.plant.CurrentGrowth/x.plant.GrowthTime>=(2.0/3.0)&&x.plant.CurrentGrowth/x.plant.GrowthTime<(1.0)){
-
-				g.drawImage(twothirdBLUEBERRY,x.xPos,x.yPos,x.Width,x.Height, null);
+			}
+			if(noSeeds==true){
+				g.setColor(Color.WHITE);
+				g.fillRect(475, 475, 120, 40);
+				g.setColor(Color.BLACK);
+				g.drawRect(475, 475, 120, 40);
+				g.setColor(Color.red);
+				g.drawString("NO SEEDS",500,500);
 
 			}
+			if(Selected.equals("Planter")){
+				g.setColor(Color.WHITE);
+				g.fillRect(375, 75, 120, 40);
+				g.setColor(Color.BLACK);
+				g.drawRect(375, 75, 120, 40);
+				g.setColor(Color.BLACK);
+				g.drawString("Planter", 400, 100);
+			}
+			if(Selected.equals("Collector")){
+				g.setColor(Color.WHITE);
+				g.fillRect(375, 75, 120, 40);
+				g.setColor(Color.BLACK);
+				g.drawRect(375, 75, 120, 40);
+				g.setColor(Color.BLACK);
+				g.drawString("Collector", 400, 100);
+			}
+			for(Button x : buttons){
+				if(x.Dim.equals("Farm")){
+					ButtonDraw(g,x);
+				}
+			}
 		}
-		for(Menu x : menu){
-			g.setColor(Color.BLUE);
-			g.fillRect(x.xPos, x.yPos, x.Width, x.Height);
-		}
-		if(noSeeds==true){
-			g.setColor(Color.WHITE);
-			g.fillRect(475, 475, 120, 40);
-			g.setColor(Color.BLACK);
-			g.drawRect(475, 475, 120, 40);
-			g.setColor(Color.red);
-			g.drawString("NO SEEDS",500,500);
-
-		}
-		if(Selected.equals("Planter")){
-			g.setColor(Color.WHITE);
-			g.fillRect(375, 75, 120, 40);
-			g.setColor(Color.BLACK);
-			g.drawRect(375, 75, 120, 40);
-			g.setColor(Color.BLACK);
-			g.drawString("Planter", 400, 100);
-		}
-		if(Selected.equals("Collector")){
-			g.setColor(Color.WHITE);
-			g.fillRect(375, 75, 120, 40);
-			g.setColor(Color.BLACK);
-			g.drawRect(375, 75, 120, 40);
-			g.setColor(Color.BLACK);
-			g.drawString("Collector", 400, 100);
-		}
-		g.setColor(Color.ORANGE);
-		g.fillRect(0,20,60,60);
-		g.drawImage(Shop, 5, 25, 50, 50, null);
+		if(Dimension=="Shop"){
+			g.setColor(Color.gray);
+			g.fillRect(0, 0, 10000, 100000);
+			for(Button x : buttons){
+				if(x.Dim.equals("Shop")){
+					ButtonDraw(g,x);
+				}
+			}
+			
 		}
 		if(start>=end){
 			stopload=true;
@@ -216,144 +240,148 @@ public class GameController extends JFrame{
 			g.setColor(Color.white);
 			g.drawString("LOADING", 500, 500);
 		}
-	
-	delay(25);
-	step();
-	repaint();
-}
-public void PlantControl(){
-	for(Spaces x : Spaces){
-		if(x.plant!=null&&x.plant.Harvestable==false){
-			x.plant.CurrentGrowth+=.1;
-			if(x.plant.CurrentGrowth>=x.plant.GrowthTime){
-				x.plant.Harvestable=true;
-				System.out.print("Harvestable : " + x.plant.Harvestable);
+
+		delay(25);
+		step();
+		repaint();
+
+	}
+	public void PlantControl(){
+		for(Spaces x : Spaces){
+			if(x.plant!=null&&x.plant.Harvestable==false){
+				x.plant.CurrentGrowth+=.1;
+				if(x.plant.CurrentGrowth>=x.plant.GrowthTime){
+					x.plant.Harvestable=true;
+					System.out.print("Harvestable : " + x.plant.Harvestable);
+				}
 			}
 		}
 	}
-}
-public void SpacesControl(){
-	Land l = Lands.get(0);
-	int num =0;
-	int numofdown=0;
-	for(Spaces x : Spaces){
-		if(num<l.Width){
-			x.xPos=l.xPos+num*CELL_SIZE;
-			x.yPos=l.yPos+numofdown*CELL_SIZE;
-		}else{
-			numofdown++;
-			num=0;
-			x.xPos=l.xPos+num*CELL_SIZE;
-			x.yPos=l.yPos+numofdown*CELL_SIZE;
+	public void SpacesControl(){
+		Land l = Lands.get(0);
+		int num =0;
+		int numofdown=0;
+		for(Spaces x : Spaces){
+			if(num<l.Width){
+				x.xPos=l.xPos+num*CELL_SIZE;
+				x.yPos=l.yPos+numofdown*CELL_SIZE;
+			}else{
+				numofdown++;
+				num=0;
+				x.xPos=l.xPos+num*CELL_SIZE;
+				x.yPos=l.yPos+numofdown*CELL_SIZE;
+			}
+			num++;
 		}
-		num++;
-	}
 
-
-}
-
-public void MouseControl(int xPos,int yPos){
-	if(yPos-60<=0){
-		Lands.get(0).yPos+=2;
-	}
-	if(xPos-60<=0){
-		Lands.get(0).xPos+=2;
-	}
-	if(yPos+100>=800){
-		Lands.get(0).yPos-=2;
-	}
-	if(xPos+60>=1000){
-		Lands.get(0).xPos-=2;
-	}
-}
-class MyMouseListener implements MouseMotionListener{
-	@Override
-	public void mouseDragged(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-	@Override
-	public void mouseMoved(MouseEvent e) {
-		// TODO Auto-generated method stub
-		MouseX=e.getX();
-		MouseY = e.getY();
-	}
-}
-class MyKeyListener implements KeyListener {
-
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
 
 	}
 
-	@Override
-	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		switch (e.getKeyCode()){
-		case KeyEvent.VK_P:Selected="Planter";
-		System.out.print("P");
-		break;
-		case KeyEvent.VK_C:Selected="Collector";	
-		System.out.print("C");
-		break;
+	public void MouseControl(int xPos,int yPos){
+		if(Dimension=="Farm"){
+			if(yPos-60<=0){
+				Lands.get(0).yPos+=2;
+			}
+			if(xPos-60<=0){
+				Lands.get(0).xPos+=2;
+			}
+			if(yPos+100>=800){
+				Lands.get(0).yPos-=2;
+			}
+			if(xPos+60>=1000){
+				Lands.get(0).xPos-=2;
+			}
 		}
 	}
-
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
-public class MouseWheelEventDemo implements MouseWheelListener{
-
-	@Override
-	public void mouseWheelMoved(MouseWheelEvent e) {
-		// TODO Auto-generated method stub
-		System.out.println(Lands.get(0).Height);
-		CELL_SIZE-=e.getWheelRotation();
-		System.out.println(Lands.get(0).Height);
-
-	}
-
-}
-class MyMouseListener2 implements MouseListener{
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
-		menus.ClickonButton(e.getX(),e.getY(), game);
-		if(Selected == "Collector"){
-			collect.CollectPlant(e.getX(), e.getY(), game);
-		}
-		if(Selected == "Planter"){
-			planter.InsertPlant(e.getX(), e.getY(), game, selectedPlant);
+	class MyMouseListener implements MouseMotionListener{
+		@Override
+		public void mouseDragged(MouseEvent e) {
+			// TODO Auto-generated method stub
 
 		}
+		@Override
+		public void mouseMoved(MouseEvent e) {
+			// TODO Auto-generated method stub
+			MouseX=e.getX();
+			MouseY = e.getY();
+		}
 	}
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
+	class MyKeyListener implements KeyListener {
+
+		@Override
+		public void keyTyped(KeyEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			// TODO Auto-generated method stub
+			switch (e.getKeyCode()){
+			case KeyEvent.VK_P:Selected="Planter";
+			System.out.print("P");
+			break;
+			case KeyEvent.VK_C:Selected="Collector";	
+			System.out.print("C");
+			break;
+			}
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			// TODO Auto-generated method stub
+
+		}
 
 	}
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
+	public class MouseWheelEventDemo implements MouseWheelListener{
+
+		@Override
+		public void mouseWheelMoved(MouseWheelEvent e) {
+			// TODO Auto-generated method stub
+			System.out.println(Lands.get(0).Height);
+			CELL_SIZE-=e.getWheelRotation();
+			System.out.println(Lands.get(0).Height);
+
+		}
 
 	}
+	class MyMouseListener2 implements MouseListener{
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			// TODO Auto-generated method stub
+			menus.ClickonButton(e.getX(),e.getY(), game);
+			but.ClickedOn(e.getX(), e.getY(), game);
+			if(Selected == "Collector"){
+				collect.CollectPlant(e.getX(), e.getY(), game);
+			}
+			if(Selected == "Planter"){
+				planter.InsertPlant(e.getX(), e.getY(), game, selectedPlant);
 
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
+			}
+		}
+		@Override
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+
+
 
 	}
-
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-
-
-}
 }
